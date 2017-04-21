082c0a43bc3af3bcc59af4db19f468e4e3200b0e
samGMIT <g00292346@gmit.ie>
2017-04-21 21:36:03 +0100

    Added timepicker and button

--- Remainder/www/js/app.js
+++ Remainder/www/js/app.js
@@ -1,5 +1,5 @@
 
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic','ionic-timepicker', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
--- Remainder/www/js/controllers.js
+++ Remainder/www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('starter.controllers', [])
 
 
 
-.controller('OneCtrl', function($scope) {
+.controller('OneCtrl', function($scope, $http) {
 	
 	 $scope.data = {};
 	$scope.response = "before ";
@@ -13,13 +13,31 @@ angular.module('starter.controllers', [])
  
         
 		$http.post(link, {username : $scope.data.username}).then(function (res){
-         $scope.response = "res.data " ;
+         $scope.response = res.data;
 			
         });
 		
   };
 })
 
-.controller('TwoCtrl', function($scope) {})
+.controller('TwoCtrl', function($scope, ionicTimePicker)  {
+	
+	$scope.openTimePicker1 = function () {
+      var ipObj1 = {
+        callback: function (val) {
+          if (typeof (val) === 'undefined') {
+            console.log('Time not selected');
+          } else {
+            var selectedTime = new Date(val * 1000);
+            console.log('Selected epoch is : ', val, 'and the time is ', selectedTime.getUTCHours(), 'H :', selectedTime.getUTCMinutes(), 'M');
+          }
+        },
+        inputTime: 0,
+        format: 12,
+        setLabel: 'Set'
+      };
+      ionicTimePicker.openTimePicker(ipObj1);
+    };
+})
 
 .controller('ThreeCtrl', function($scope) {});
